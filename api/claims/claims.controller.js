4544bcde67fd9c2ebf4702631cbbb762e57cb0a2
Juan Andrés <jasrd1990@gmail.com>
2018-05-10 03:26:57 +0100

    masclaims

--- api/claims/claims.controller.js
+++ api/claims/claims.controller.js
@@ -2,6 +2,7 @@
 
 var claims = [
     {   
+    	_id:"der231dq",
         czone:1,
         creator:1,
         title : 'Estoy harta del vecino',
@@ -10,17 +11,39 @@ var claims = [
         body: 'Mira yo ya no puedo mas, todos los dias mi vecino sale de casa, pasa por delante del jardin y me pisa las flores'+
               'Me dejan matarlo? Un poquito solo, para ver si se asusta y pilla el mensaje',
         votes : 0,
-        resources: 'http://i0.kym-cdn.com/entries/icons/original/000/002/232/bullet_cat.jpg'
+        resources:['http://i0.kym-cdn.com/entries/icons/original/000/002/232/bullet_cat.jpg','https://i.redd.it/gyoipjj9d60z.jpg','http://epicpix.com/wp-content/uploads/2014/06/ff_1296.jpg']
 
     },
-    {
+    {	_id:"der231dq",
         title: 'La calle está llena de mierda',
         receiver: 'Don Saltarin',
         resume: 'Los perritos me dejan las cacas delante de casa, estoy por ir a cagarle a sus dueños en la boca a ver',
         body: 'La gente es super guarra, todos los dias me encuentro por la calle cacas de perro por todos lado y estoy hartandome' +
               ' Estoy por poner una camara para identificar a los dueños de los perros, ir  sus casas y cagarles en la boca mientras duermen',
         votes: 10,
-        resources: 'http://i0.kym-cdn.com/entries/icons/original/000/002/232/bullet_cat.jpg'
+        resources:['http://i0.kym-cdn.com/entries/icons/original/000/002/232/bullet_cat.jpg','https://i.redd.it/gyoipjj9d60z.jpg','http://epicpix.com/wp-content/uploads/2014/06/ff_1296.jpg']
+
+        },
+        {
+        _id:"der231dq",
+        title: 'La calle está llena de mierda',
+        receiver: 'Don Saltarin',
+        resume: 'Los perritos me dejan las cacas delante de casa, estoy por ir a cagarle a sus dueños en la boca a ver',
+        body: 'La gente es super guarra, todos los dias me encuentro por la calle cacas de perro por todos lado y estoy hartandome' +
+              ' Estoy por poner una camara para identificar a los dueños de los perros, ir  sus casas y cagarles en la boca mientras duermen',
+        votes: 10,
+        resources:['http://i0.kym-cdn.com/entries/icons/original/000/002/232/bullet_cat.jpg','https://i.redd.it/gyoipjj9d60z.jpg','http://epicpix.com/wp-content/uploads/2014/06/ff_1296.jpg']
+
+        },
+        {
+        id:"der231dq",
+        title: 'La calle está llena de mierda',
+        receiver: 'Don Saltarin',
+        resume: 'Los perritos me dejan las cacas delante de casa, estoy por ir a cagarle a sus dueños en la boca a ver',
+        body: 'La gente es super guarra, todos los dias me encuentro por la calle cacas de perro por todos lado y estoy hartandome' +
+              ' Estoy por poner una camara para identificar a los dueños de los perros, ir  sus casas y cagarles en la boca mientras duermen',
+        votes: 10,
+        resources:['http://i0.kym-cdn.com/entries/icons/original/000/002/232/bullet_cat.jpg','https://i.redd.it/gyoipjj9d60z.jpg','http://epicpix.com/wp-content/uploads/2014/06/ff_1296.jpg']
 
         },
 ]
